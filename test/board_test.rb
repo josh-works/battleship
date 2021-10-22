d2dadd48496609b949a33e2b49692df0987e5ff2
Josh Thompson <thompsonjoshd@gmail.com>
2021-10-22 15:56:18 -0600

    Access hash correctly

---
+++ test/board_test.rb
@@ -0,0 +1,14 @@
+require './test/test_helper'
+
+class BoardTest < Minitest::Test
+  def setup
+    @board = Board.new
+  end
+  
+  def test_cells_returns_a_formatted_hash
+    assert_instance_of Hash, @board.cells
+    assert_equal :A1, @board.cells.first[0]
+    assert_instance_of Cell, @board.cells.first
+  end
+  
+end
