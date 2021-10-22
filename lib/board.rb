5ee052ede01cfd5241a10882d1199998f005a94b
Josh Thompson <thompsonjoshd@gmail.com>
2021-10-22 15:48:08 -0600

    slight refactor

--- lib/board.rb
+++ lib/board.rb
@@ -1,13 +1,16 @@
 class Board
   attr_reader :cells
   def initialize()
-    @cells = build_cells
+    @cells = {}
+    build_cells
   end
   
   def build_cells
+    results = {}
     ("a".."d").each_with_index do |letter, num|
       coord = letter + num.to_s
-      @cells[coord] = Cell.new(coord)
+      require "pry"; binding.pry
+      results[coord] = Cell.new(coord) 
     end
   end
 end
