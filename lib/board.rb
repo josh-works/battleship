303e0e7d5e829af9539b4c1ab42c42faa7b20b2b
Josh Thompson <thompsonjoshd@gmail.com>
2021-10-22 16:00:11 -0600

    +1 index and access results array better

--- lib/board.rb
+++ lib/board.rb
@@ -7,7 +7,7 @@ class Board
   
   def build_cells
     ("a".."d").each_with_index do |letter, num|
-      coord = letter.upcase + num.to_s
+      coord = letter.upcase + (num + 1).to_s
       @cells[coord] = Cell.new(coord) 
     end
   end
--- test/board_test.rb
+++ test/board_test.rb
@@ -8,7 +8,7 @@ class BoardTest < Minitest::Test
   def test_cells_returns_a_formatted_hash
     assert_instance_of Hash, @board.cells
     assert_equal "A1", @board.cells.first[0]
-    assert_instance_of Cell, @board.cells.first
+    assert_instance_of Cell, @board.cells.first[1]
   end
   
 end
